9b4e23fee81e059d116fedf44b779dffdfab2b91
Matthew Harvey <matthewtlharvey@gmail.com>
2023-03-16 21:08:12 +0000

    t3 starter with openai connection

---
+++ src/server/api/routers/example.ts
@@ -0,0 +1,53 @@
+/* eslint-disable @typescript-eslint/no-for-in-array */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+
+import { z } from "zod";
+import {Configuration, OpenAIApi} from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "~/server/api/trpc";
+
+export const exampleRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.example.findMany();
+  }),
+
+  getSecretMessage: protectedProcedure.query(() => {
+    return "you can now see this secret message!";
+  }),
+
+  chatGPT: publicProcedure
+    .query(async ({}) => {
+      const openai = new OpenAIApi(configuration);
+
+      const messages = [];
+      messages.push({ role: "user", content: "Hello again!" });
+
+      // @ts-ignore
+      const completion = await openai.createChatCompletion({model: "gpt-3.5-turbo",messages: messages});
+
+      const text = completion.data.choices[0]?.message;
+
+      return {
+        // @ts-ignore
+        api_test: text,
+      };
+    }),
+
+});
